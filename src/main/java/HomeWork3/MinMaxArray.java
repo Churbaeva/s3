8378bea854c0fcd9f1c856aeb8f12f9925e1105d
churbaeva <fedosova1997@mail.ru>
2023-06-09 10:18:43 +0500

    Initial commit

---
+++ src/main/java/HomeWork3/MinMaxArray.java
@@ -0,0 +1,45 @@
+package HomeWork3;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class MinMaxArray {
+    public void ArrayFunction() {
+        ArrayList<Integer> array = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите размер списка: ");
+        int sizeArray = in.nextInt();
+        System.out.println("Введите максимальное число для списка: ");
+        int arrayMax = in.nextInt();
+        Random random = new Random();
+        for (int i = 0; i < sizeArray; i++) {
+            array.add(random.nextInt(arrayMax + 1));
+        }
+        System.out.println("Первичный список: ");
+        System.out.println(array);
+        System.out.println("Максимальный элемент в списке: " + Collections.max(array));
+        System.out.println("Минимальный элемент в списке: " + Collections.min(array));
+        int summaArray = 0;
+        for (int i = 0; i < array.size(); i++) {
+            summaArray = summaArray + array.get(i);
+        }
+        System.out.println("Среднее значение в списке: " + summaArray / sizeArray);
+        array.removeIf(i -> i % 2 == 0);
+        System.out.println(array);
+        ArrayList<Integer> evenArray = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++) {
+            {
+                evenArray.add(array.get(i));
+            }
+        }
+        System.out.println("Вторичный список:" + evenArray);
+        System.out.println("Максимальный элемент в списке: " + Collections.max(evenArray));
+        System.out.println("Минимальный элемент в списке: " + Collections.min(evenArray));
+        int summaEvenArray = 0;
+        for (int i = 0; i < evenArray.size(); i++) {
+            summaEvenArray = summaEvenArray + evenArray.get(i);
+        }
+        System.out.println("Среднее значение в списке: " + summaEvenArray / evenArray.size());
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+
+import Homework3.MinMaxArray;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MinMaxArray work1 = new MinMaxArray();
+        work1.ArrayFunction();
+    }
+}
